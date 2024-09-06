f75adfb338b69bad47b1159c70bed5c54aa7bd4c
Adam Bauer <asbauer@ucsd.edu>
2024-09-06 00:20:39 -0700

    Added resume tab and favicon

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,59 @@
+ 
+ <h2>Under Construction...</h2>
+ 
+
+
+
+<style>
+
+    
+    h2 {
+        text-align: center;
+    }
+
+    h4{ 
+       margin-bottom: 2px;
+       margin-top: 2px;
+       
+   }
+   .summary {
+       display: inline;
+       text-decoration: none;
+      
+   }
+
+   h4.progress-subheader{
+       text-decoration: none;
+       
+   }
+
+   p.progress-text {
+       margin-top: 0;
+       margin-bottom: 0px;
+   }
+
+   ul, ol {
+       padding-left: 1%;
+   }
+
+   .overview {
+       list-style-type: none;
+   }
+
+   #education+ul{ 
+       cursor: default;
+
+   }
+
+   h5 {
+       margin: 1% 0 1% 0;
+   }
+
+   
+   
+
+  
+  
+   
+ 
+</style>
